c75ed312218f2d67b14bf53fb84094e4aae9e416
Wim Stalmans <wim.stalmans@gmail.com>
2024-07-13 07:25:19 +0200

    initial scaffolding for smart contract

---
+++ contracts/Proxy.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.24;
+
+// import "hardhat/console.sol";
+
+contract Proxy {
+
+}
+
+contract Logic1 {
+
+}
+
+contract Logic2 {
+    
+}
